aab1d35809e7a2135671edb24e957b7b7b446124
GolfKamonchanok <kamonchanok.201143@gmail.com>
2021-11-29 21:15:22 +0700

    ืnotification

--- lib/pages/notification/components/body.dart
+++ lib/pages/notification/components/body.dart
@@ -7,6 +7,40 @@ class BodyNotification extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     /* https://api.flutter.dev/flutter/material/ListTile-class.html */
-    return Background(child: Text("Notification page"));
+Size size = MediaQuery.of(context).size;
+    return Background(
+        child: Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.only(top: 15),
+          child: Column(
+            children: [
+              SizedBox(
+                width: size.width * 0.95,
+                  height: size.height * 0.10,
+                child: Card(
+                  child: ListTile(
+                    leading: ConstrainedBox(
+                      constraints: const BoxConstraints(
+                        minWidth: 44,
+                        minHeight: 44,
+                        maxWidth: 64,
+                        maxHeight: 64,
+                      ),
+                      child: Image.asset('assets/images/person.png'),
+                    ),
+                    title: const Text('ศูนย์บริการนักศึกษา'),
+                    subtitle: const Text(
+                      'กำลังจะถึงคิวของคุณแล้ว',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    ));
   }
 }
