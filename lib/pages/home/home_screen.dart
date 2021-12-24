121e68d33aa66dd64529ca1cdf69f5b128adb05c
nuttawanice <610107030028@dpu.ac.th>
2021-12-24 12:25:11 +0700

    push Search

--- lib/pages/home/home_screen.dart
+++ lib/pages/home/home_screen.dart
@@ -48,6 +48,15 @@ class _HomeScreenState extends State<HomeScreen> {
           backgroundColor: kPrimaryColor,
           actions: [
             IconButton(
+              onPressed: () {
+                showSearch(
+                  context: context,
+                  delegate: CustomSearchDelegate(),
+                );
+              },
+              icon: const Icon(Icons.search, color: Colors.white),
+            ),
+            IconButton(
                 onPressed: () {
                   Navigator.push(
                       context,
@@ -129,5 +138,81 @@ class _HomeScreenState extends State<HomeScreen> {
             });
           },
         ));
+  }
+}
+
+class CustomSearchDelegate extends SearchDelegate {
+  // List<String> searchTerms = [
+  //   'Momo',
+  //   'ถ่ายเอกสาร',
+  //   'ห้องสมุด',
+  // ];
+
+  List<String> searchTerms = [];
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var fruit in searchTerms) {
+      if (fruit.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(fruit);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+            title: Text(result),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => ProfileuserScreen()));
+            });
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var fruit in searchTerms) {
+      if (fruit.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(fruit);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+            title: Text(result),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => ProfileuserScreen()));
+            });
+      },
+    );
   }
 }
